83cec8499b9fee7407fbef953b5df95e9386607e
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-09-15 09:24:00 +0200

    linter

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -1,5 +1,5 @@
 /** Convert a string to camel case format */
-export function toCamelCase(str: string) {
+export function toCamelCase(str: string): string {
   return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match: string, index: number) {
     if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
     return index === 0 ? match.toLowerCase() : match.toUpperCase();
